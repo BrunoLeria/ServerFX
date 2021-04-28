d646d146f77879dfaaa359d10e7f1e7a255cc346
Bruno <bruno.acerteaqui@gmail.com>
2021-04-28 19:57:28 -0300

    Adicionado novos campos tipo, ip e porta, e funcionalidades de cada campo, onde o campo tipo é cada tipo de usuário(Usuário, Médico, Admin), e os campos ip e porta para possibilitar conectar com um diferente das padrões.

--- src/chat/ClientApp.java
+++ src/chat/ClientApp.java
@@ -1,82 +1,143 @@
 package chat;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Parent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 import org.json.JSONObject;
 
 public class ClientApp extends Application {
-
-    private TextArea messages = new TextArea();
-    private NetworkConnection connection = createClient();    
-    private JSONObject json = new JSONObject();
-
-    private Parent createContent() {
-        messages.setFont(Font.font(36));
-        messages.setPrefHeight(550);
-        messages.setEditable(false);
-
-        TextField input = new TextField();
-        input.setOnAction(event -> {
-            String message = "Client: " + input.getText();
-            input.clear();
-
-            messages.appendText(message + "\n");
-
-            json.append("message", message);
-
-            DataPacket packet = new DataPacket(
-                    new Encryptor().enc(json.toString().getBytes())
-            );
-
-
-            try {
-                connection.send(packet);
-            }
-            catch (Exception e) {
-                messages.appendText("Failed to send\n");
-            }
-        });
-
-        VBox root = new VBox(20, messages, input);
-        root.setPrefSize(600, 600);
-        return root;
-    }
+   
+    @FXML
+    private TextField user = new TextField();
+    @FXML
+    private ChoiceBox type;
+    @FXML
+    private TextField port = new TextField("8899");;
+    @FXML
+    private TextField ip = new TextField("127.0.0.1");
+    private NetworkConnection connection;
+    private URL url;
+    private Stage pStage;
 
     @Override
-    public void init() throws Exception {
-        connection.startConnection();
-    }
+    public void init(){
+        //TODO
+    }  
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(createContent()));
-        primaryStage.show();
+        url = new File("src/chat/Client.fxml").toURI().toURL();
+        
+        type = new ChoiceBox();
+        
+        type.getItems().add("Usuario");
+        type.getItems().add("Médico");
+        type.getItems().add("Admin");   
+        
+        primaryStage.setScene(new Scene(FXMLLoader.load(url)));
+        
+        setPrimaryStage(primaryStage);
+        pStage = primaryStage;
+        
+        primaryStage.show();            
     }
-
+    
     @Override
     public void stop() throws Exception {
         connection.closeConnection();
     }
 
-    private Client createClient() {
-        return new Client("127.0.0.1", 8899, data -> {
+    public void iniciar() throws Exception{
+        int porta = Integer.parseInt(port.getText());
+        
+        
+        
+        connection.startConnection();
+        
+        cadastrar();
+        
+        connection = new Client(ip.getText(), porta, data -> {
             DataPacket packet = (DataPacket) data;
             byte[] original = new Encryptor().dec(packet.getRawBytes());
 
             Platform.runLater(() -> {
-                messages.appendText(new String(original) + "\n");
+                user.appendText(new String(original) + "\n");
             });
-        });             
+        });
+
+        JSONObject recebido = new JSONObject(connection.getInputStream());
+        switch(recebido.get("cod").toString()){
+            case "11":
+                url = new File("src/chat/Formulario.fxml").toURI().toURL();
+                getPrimaryStage().setScene(new Scene(FXMLLoader.load(url)));
+                getPrimaryStage().show();   
+                JSONObject resposta = new JSONObject();
+                break;
+            case "5":
+                connection.closeConnection();
+                System.exit(1);
+                break;
+            case "8":
+                if(recebido.get("covid").toString().equals("true")){
+                    url = new File("src/chat/CovidPos.fxml").toURI().toURL();
+                    getPrimaryStage().setScene(new Scene(FXMLLoader.load(url)));
+                    getPrimaryStage().show();
+                }else{
+                    url = new File("src/chat/CovidNeg.fxml").toURI().toURL();
+                    getPrimaryStage().setScene(new Scene(FXMLLoader.load(url)));
+                    getPrimaryStage().show();
+                }
+                break;
+            default:
+                connection.closeConnection();                       
+                System.exit(1);
+                break;
+        }      
     }
+    
+    private void cadastrar(){
+        try {
+            JSONObject cadastro = new JSONObject();
+	    cadastro.put("nome", user.getText());
+	    cadastro.put("tipo", type.getValue());
+            cadastro.put("cod", "1");
+            String cadastroString = cadastro.toString();
+            
+            JOptionPane.showMessageDialog(null,"Dados recebidos:" + cadastroString);
+            
+            connection.send(cadastro);
+            System.out.println(cadastroString);
+            
+        }catch(Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+    
+    public Stage getPrimaryStage() {
+        return pStage;
+    }
+
+    private void setPrimaryStage(Stage pStage) {
+        this.pStage = pStage;
+    }
+
+    
     
     public static void main(String[] args) {
         launch(args);
     }
+    
+    
 }
